ed4a6d823be887d6bf4cc3cdf81eb28e91214183
Ray Hung <howard88tw@gmail.com>
2020-09-09 13:59:54 -0400

    move parseInput() to the top

--- generateAns.py
+++ generateAns.py
@@ -1,23 +1,3 @@
-# back tracking algorithm
-def generateCombinations(curIndex, elements, curAns, possibleAns):
-    # base case 
-    if curIndex >= len(elements):
-        possibleAns.append("[" + "".join(curAns) + "]")
-        possibleAns.append("[" + "".join(curAns) + " ]")
-        return
-    
-    if len(curAns) == 0 or curAns[-1] != ' ':
-        # add a space 
-        curAns.append(' ')
-        generateCombinations(curIndex, elements, curAns, possibleAns)
-        curAns.pop()
-    
-    if len(curAns) == 0 or elements[curIndex] == ';' or not curAns[-1].replace('.','',1).isdigit():
-        # do not add a space
-        curAns.append(elements[curIndex])
-        generateCombinations(curIndex + 1, elements, curAns, possibleAns)
-        curAns.pop()
-
 def parseInput(inputStr):
     # get rid of '[' ']'
     inputStr = inputStr[1:-1]
@@ -43,6 +23,25 @@ def parseInput(inputStr):
 
     return elements
 
+# back tracking algorithm
+def generateCombinations(curIndex, elements, curAns, possibleAns):
+    # base case 
+    if curIndex >= len(elements):
+        possibleAns.append("[" + "".join(curAns) + "]")
+        possibleAns.append("[" + "".join(curAns) + " ]")
+        return
+    
+    if len(curAns) == 0 or curAns[-1] != ' ':
+        # add a space 
+        curAns.append(' ')
+        generateCombinations(curIndex, elements, curAns, possibleAns)
+        curAns.pop()
+    
+    if len(curAns) == 0 or elements[curIndex] == ';' or not curAns[-1].replace('.','',1).isdigit():
+        # do not add a space
+        curAns.append(elements[curIndex])
+        generateCombinations(curIndex + 1, elements, curAns, possibleAns)
+        curAns.pop()
 
 if __name__ == '__main__':
     ans = input("Please enter the ans as MATLAB notation without any unnecessary space or commas. E.g. [3;5;6]\n") 
