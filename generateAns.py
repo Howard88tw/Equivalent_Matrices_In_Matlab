1709b04596dbe6e504100217316092e96b8732c9
Ray Hung <howard88tw@gmail.com>
2020-09-16 17:52:51 -0400

    more documentation

--- generateAns.py
+++ generateAns.py
@@ -1,16 +1,20 @@
 def parseInput(inputStr):
-    # get rid of '[' ']'
+    # get rid of leading '[' and trailing ']'
     inputStr = inputStr[1:-1]
-
+    
     elements = []    
     curElement = []
 
     for c in inputStr:
-        if c.isdigit() or c == '-' or c == '.':
+        # '-' for negative number
+        # '.' for floating point number
+        if c.isdigit() or c == '-' or c == '.': 
             curElement.append(c)
+        # entries in the same row are separated by one space
         elif c == ' ':
             elements.append("".join(curElement))
             curElement = []
+        # entries in the different rows are separated by ';'
         elif c == ';':
             elements.append("".join(curElement))
             elements.append(';')
@@ -24,24 +28,30 @@ def parseInput(inputStr):
     return elements
 
 # back tracking algorithm
-def generateCombinations(curIndex, elements, curAns, possibleAns):
+def generateCombinations(curIndex, elements, curCombination, possibleAns):
+    '''
+    curIndex - the current index of the element that is being processed
+    elements - a list of elements got from parseInput()
+    curCombination - a list of elements as the current combination
+    possibleAns - a list storing all possible combinations
+    '''
     # base case 
     if curIndex >= len(elements):
-        possibleAns.append("[" + "".join(curAns) + "]")
-        possibleAns.append("[" + "".join(curAns) + " ]")
+        possibleAns.append("[" + "".join(curCombination) + "]")
+        possibleAns.append("[" + "".join(curCombination) + " ]")
         return
     
-    if len(curAns) == 0 or curAns[-1] != ' ':
-        # add a space 
-        curAns.append(' ')
-        generateCombinations(curIndex, elements, curAns, possibleAns)
-        curAns.pop()
+    # case 1: can add a space  
+    if len(curCombination) == 0 or curCombination[-1] != ' ':
+        curCombination.append(' ')
+        generateCombinations(curIndex, elements, curCombination, possibleAns)
+        curCombination.pop()
     
-    if len(curAns) == 0 or elements[curIndex] == ';' or not curAns[-1].replace('.','',1).isdigit():
-        # do not add a space
-        curAns.append(elements[curIndex])
-        generateCombinations(curIndex + 1, elements, curAns, possibleAns)
-        curAns.pop()
+    # case 1: cannot add a space. Add the next element
+    if len(curCombination) == 0 or elements[curIndex] == ';' or not curCombination[-1].replace('.','',1).isdigit():
+        curCombination.append(elements[curIndex])
+        generateCombinations(curIndex + 1, elements, curCombination, possibleAns)
+        curCombination.pop()
 
 if __name__ == '__main__':
     ans = input("Please enter the matrix as MATLAB notation without any unnecessary space or commas. E.g. [3;5;6]\n") 
